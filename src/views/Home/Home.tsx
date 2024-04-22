4f8f46b158d710cb7822f6cc83e6eb1af206d086
Herbert Kirakos <kirakosyanherbertdev@gmail.com>
2024-04-22 19:25:47 +0400

    updates

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,6 +6,7 @@ import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 import { DataType } from '@/app/page';
 import { Portfolio } from './Portfolio';
+import { Instagram } from './Instagram';
 
 const getContactIcon = (type: string) => {
   switch(type) {
@@ -119,41 +120,7 @@ export const Home = async ({data}: {data: DataType}) => {
       </Container>
 
       {/* Instagram */}
-      <Container disableGutters maxWidth="xl" sx={ { px: 1, mt: { xs: 8, sm: 14 } }}>
-        <Stack justifyContent="center" mb={4}>
-          <Typography variant="h4" align="center" textTransform="uppercase" letterSpacing={2} fontWeight={300}>
-            Instagram
-          </Typography>
-          <Typography variant="subtitle2" align="center" fontWeight={600} sx={{ textDecoration: 'underline'}}>
-            {instagram.nickname}
-          </Typography>
-        </Stack>
-
-        <Grid container spacing={0.5}>
-          {
-            instagram.media.map((item) => (
-              <Grid key={`item-${item.id}`} item xs={6} sm={4} md={3} lg={2}>
-                <Image
-                  key={`item-${item.id}`}
-                  alt={item.alt}
-                  src={item.url || ''}
-                  width={220}
-                  height={220}
-                  style={{
-                    minWidth: '100%',
-                    minHeight: '100%',
-                    maxWidth: '100%',
-                    maxHeight: '100%',
-                    // height: '600px',
-                    // width: '384px',
-                    objectFit: 'cover',
-                  }}
-                />
-              </Grid>
-            ))
-          }
-        </Grid>
-      </Container>
+      <Instagram data={data}/>
 
       {/* Contact */}
       <Container maxWidth="xl" sx={{ backgroundColor: 'white', py: { xs: 8, sm: 14 }}}>
